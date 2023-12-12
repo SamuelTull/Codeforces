f1ab5c14818bf8e5268cfaed281002f1b9d15de5
SamuelTull <samueltull9@gmail.com>
2023-12-12 17:52:13 +0000

    1324F recursion

---
+++ practice/1324F.py
@@ -0,0 +1,36 @@
+import os
+from io import BytesIO
+import sys
+
+input = BytesIO(os.read(0, os.fstat(0).st_size)).readline
+ans = []
+
+
+def dfs(u, p):  # DP[u]=max for tree rooted at u
+    DP[u] = nums[u]
+    for v in adj[u]:
+        if v != p:
+            dfs(v, u)
+            DP[u] += max(0, DP[v])
+
+
+def dfs2(u, p):  # ans[u]=max for tree including u (not neccesarily rooted at)
+    ans[u] += max(0, ans[p] - max(0, DP[u]))
+    for v in adj[u]:
+        if v != p:
+            dfs2(v, u)
+
+
+n = int(input())
+nums = [1 if int(x) == 1 else -1 for x in input().split()]
+adj = [[] for _ in range(n)]
+for _ in range(n - 1):
+    u, v = map(int, input().split())
+    adj[u - 1].append(v - 1)
+    adj[v - 1].append(u - 1)
+DP = [0 for _ in range(n)]
+dfs(0, -1)
+ans = [DP[i] for i in range(n)]
+for u in adj[0]:
+    dfs2(u, 0)
+os.write(1, "\n".join(map(str, ans)).encode())
