dc3fa6b6512e132654fbc035b9af09dc41d337db
SamuelTull <samueltull9@gmail.com>
2023-12-16 22:24:33 +0000

    915

---
+++ round915/c.py
@@ -0,0 +1,31 @@
+import os
+from io import BytesIO
+
+input = BytesIO(os.read(0, os.fstat(0).st_size)).readline
+ans = []
+
+
+for _ in range(int(input())):
+    n = int(input())
+    string = list(input().decode().rstrip())
+    substring = []
+    for i in range(n):
+        while substring and string[substring[-1]] < string[i]:
+            substring.pop()
+        substring.append(i)
+
+    m = len(substring)
+    for i in range(m // 2):
+        i1 = substring[i]
+        i2 = substring[m - 1 - i]
+        string[i1], string[i2] = string[i2], string[i1]
+    if string == sorted(string):
+        s = m
+        i = 0
+        while s and string[substring[0]] == string[substring[i]]:
+            s -= 1
+            i += 1
+        ans.append(str(s))
+    else:
+        ans.append("-1")
+os.write(1, "\n".join(ans).encode())
