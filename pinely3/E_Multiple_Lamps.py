86f360f964e556df3f2d1d105381f8c1ee6a8901
SamuelTull <samueltull9@gmail.com>
2023-12-23 21:57:55 +0000

    bit - AA - E

--- pinely3/E_Multiple_Lamps.py
+++ pinely3/E_Multiple_Lamps.py
@@ -8,13 +8,13 @@ poss = {}
 
 def how(p):
     # we want to turn on all of p
-    state = [False] * 20
-    switch = set()
+    state = 0  # All switches are initially off
+    switch = 0  # No switches have been switched yet
     for i in range(1, 20):
-        if (i in p) != state[i]:
-            switch.add(i)
+        if (i in p) != bool(state & (1 << i)):
+            switch |= 1 << i  # Switch the i-th switch
             for j in range(i, 20, i):
-                state[j] = not state[j]
+                state ^= 1 << j  # Toggle the j-th switch
     return switch
 
 
@@ -25,13 +25,12 @@ for i in range(1, 20):
         for k in range(j + 1, 20):
             poss[(i, j, k)] = how((i, j, k))
 
-
 for _ in range(int(input())):
     n, m = map(int, input().split())
-    children = {i: set() for i in range(n + 1)}
+    children = [0] * (n + 1)
     for _ in range(m):
         i, j = [int(x) for x in input().split()]
-        children[i].add(j)
+        children[i] |= 1 << j
     if int(sqrt(n)) <= n // 5:
         print(n)
         print(" ".join(str(x + 1) for x in range(n)))
@@ -40,10 +39,14 @@ for _ in range(int(input())):
         for p, switch in poss.items():
             if max(p) > n or len(p) > n // 5:
                 continue
-            switch = switch & set(range(1, n + 1))
-            if all(children[i] <= switch for i in switch):
-                print(len(switch))
-                print(" ".join(str(x) for x in switch))
+            switch &= (1 << (n + 1)) - 1
+            child = 0
+            for i in range(1, n + 1):
+                if switch & (1 << i):
+                    child |= children[i]
+            if child & switch == child:
+                print(bin(switch).count("1"))
+                print(" ".join(str(x) for x in range(1, n + 1) if switch & (1 << x)))
                 break
         else:
             print(-1)
