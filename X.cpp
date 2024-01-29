4e7cb2921aaa6cac48932f78b40f22f068246cd0
SamuelTull <samueltull9@gmail.com>
2024-01-29 13:43:21 +0000

    refactor x

--- X.cpp
+++ X.cpp
@@ -1,31 +1,26 @@
 // clang-format off
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp> 
-#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
 #define int long long
 const int MOD = 998244353;
-const int INF = 1e18;
-// const int INF = 1e9;
-#define dbg(x)cout<<(#x)<<": [";for(auto i=x.begin();i!=x.end();++i)cout<<*i<<(next(i)!=x.end()?", ":"");cout<<"]\n"; // container 
-#define dbgm(x)cout<<(#x)<<": [";for(auto i=x.begin();i!=x.end();++i)cout<<"("<<i->first<<", " << i->second <<(next(i)!=x.end()?"), ":")");cout<<"]\n"; // map or container<pair>
-#define dbgv(x,n)cout<<(#x)<<": [";for(auto i=x.begin();i!=x.end();++i){cout<<"(";int j=n;while(j--){cout<<(*i)[j]<<(j==0?"":", ");};cout<<(next(i)!=x.end()?"), ":")");};cout<<"]\n"; // vector of vectors
-#define dbgvar(x)cout<<(#x)<<": "<<x<<"\n"; // for variables 
-#define dbgpair(x)cout<<(#x)<<": ("<<x.first<<", "<<x.second<<")\n"; // for pairs
-template <typename T>
-vector<pair<T,int>>getStreaks(vector<T> a){vector<pair<T,int>>streaks;int n=a.size();int i=0;while(i<n){int j=i;while(j<n&&a[j]==a[i])j++;streaks.push_back({a[i],j-i});i=j;}return streaks;}
-vector<pair<char,int>>getStreaks(string s){return getStreaks(vector<char>(s.begin(),s.end()));}
-// ordered_set
-using ordered_set = __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>; // order_of_key(a);find_by_order(n)
-// clang-format on
+const int INF = 1e18; // 1e9
+const int MAXBIT = 62; //30
 
+#define dbg(x)cout<<(#x)<<": [";for(auto qq=x.begin();qq!=x.end();++qq)cout<<*qq<<(next(qq)!=x.end()?", ":"");cout<<"]\n"; // container 
+#define mbg(x)cout<<(#x)<<": [";for(auto qq=x.begin();qq!=x.end();++qq)cout<<"("<<qq->first<<", "<<qq->second<<(next(qq)!=x.end()?"), ":")");cout<<"]\n"; // map or container<pair>
+#define vbg(x)cout<<(#x)<<": [";for(auto qq=x.begin();qq!=x.end();++qq){cout<<"(";for(auto qqq=qq->begin();qqq!=qq->end();qqq++){cout<<*qqq<<(next(qqq)!=qq->end()?", ":"");};cout<<(next(qq)!=x.end()?"), ":")");};cout<<"]\n"; // vector of vectors
+#define xbg(x)cout<<(#x)<<": "<<x<<"\n"; // for variables 
+#define pbg(x)cout<<(#x)<<": ("<<x.first<<", "<<x.second<<")\n"; // for pairs
+// clang-format on
 void solve()
 {
     int n, num;
     cin >> n;
     vector<int> a(n);
     for (int i = 0; i < n; i++)
-        {cin >> a[i];}
+    {
+        cin >> a[i];
+    }
 }
 
 signed main()
@@ -36,8 +31,8 @@ signed main()
     int t = 1;
     cin >> t;
     while (t--)
+        // cout << solve() << "\n";
+        // cout << (solve() ? "YES" : "NO") << "\n";
         solve();
-    // cout << solve() << "\n";
-    // cout << (solve() ? "YES" : "NO") << "\n";
     return 0;
 }
--- utils.cpp
+++ utils.cpp
@@ -3,8 +3,13 @@
 using namespace std;
 
 // ordered_set
-#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
+using ordered_set = __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>; // order_of_key(a);find_by_order(n)
+
+template <typename T>
+vector<pair<T,int>>getStreaks(vector<T> a){vector<pair<T,int>>streaks;int n=a.size();int i=0;while(i<n){int j=i;while(j<n&&a[j]==a[i])j++;streaks.push_back({a[i],j-i});i=j;}return streaks;}
+vector<pair<char,int>>getStreaks(string s){return getStreaks(vector<char>(s.begin(),s.end()));}
 using ordered_set = __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>; // order_of_key(a);find_by_order(n)
 
 // priority_queue
@@ -30,4 +35,54 @@ vector<int> sieveOfEratosthenes(int N)
             primes.push_back(p);
 
     return primes;
+}
+
+/// modular arithemtic
+// a / b  = a * inverse(b) % MOD
+
+int power(int a, int b)
+// The power function uses the binary exponentiation algorithm to efficiently calculate a^b % MOD. It represents b in binary and multiplies res by a whenever it encounters a 1 in the binary representation of b. It also squares a in each iteration. This allows it to calculate a^b % MOD in O(log b) time.
+{
+    int res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+        {
+            res = res * a % MOD;
+        }
+        a = a * a % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+
+int inverse(int a)
+// The inverse function is using Fermat's Little Theorem, which states that if p is a prime number and a is an integer that is not divisible by p, then a^(p-1) ≡ 1 (mod p). If we multiply both sides by a^-1, we get a^(p-2) ≡ a^-1 (mod p). This means that a^(p-2) is the multiplicative inverse of a under modulo p.
+// The inverse function calculates a^(MOD - 2) % MOD, which is the multiplicative inverse of a under modulo MOD, using the power function.
+
+{
+    return power(a, MOD - 2);
+}
+
+const int N = 200005;
+int fact[N], inv[N];
+void pre()
+{
+    fact[0] = 1;
+    inv[0] = 1;
+    for (int i = 1; i < N; i++)
+        fact[i] = (i * fact[i - 1]) % MOD;
+    for (int i = 1; i < N; i++)
+        inv[i] = power(fact[i], MOD - 2);
+}
+
+int nCr(int n, int r)
+{
+    if (r > n || r < 0)
+        return 0;
+    if (n == r)
+        return 1;
+    if (r == 0)
+        return 1;
+    return (((fact[n] * inv[r]) % MOD) * inv[n - r]) % MOD;
 }
